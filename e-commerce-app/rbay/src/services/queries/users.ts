a84deca19549d5ca1376e9ca38ff2a3d17350eee
Noah Kim <dongwoo_333@hanmail.net>
2023-05-11 08:08:00 +1000

    47. Fetching a User

--- e-commerce-app/rbay/src/services/queries/users.ts
+++ e-commerce-app/rbay/src/services/queries/users.ts
@@ -5,7 +5,11 @@ import { usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
@@ -17,6 +21,14 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 const serialize = (user: CreateUserAttrs) => {
 	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
 		username: user.username,
 		password: user.password
 	};
