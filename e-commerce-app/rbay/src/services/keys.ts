0686d5d0669874a63ad4fc71c7688a522f4fced4
Noah Kim <dongwoo_333@hanmail.net>
2023-05-12 16:47:41 +1000

    66. Requiring Unique User Names

--- e-commerce-app/rbay/src/services/keys.ts
+++ e-commerce-app/rbay/src/services/keys.ts
@@ -2,3 +2,4 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const usernamesUniqueKey = 'usernames:unique';
--- e-commerce-app/rbay/src/services/queries/users.ts
+++ e-commerce-app/rbay/src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -14,7 +14,17 @@ export const getUserById = async (id: string) => {
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
 
+	// See if the username is already in the set of usernames
+	const exists = await client.sIsMember(usernamesUniqueKey, attrs.username);
+
+	// If so, throw an error
+	if (exists) {
+		throw new Error('Username is taken');
+	}
+	// Otherwise, continue
+
 	await client.hSet(usersKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey, attrs.username);
 
 	return id;
 };
