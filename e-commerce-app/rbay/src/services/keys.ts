e9babef7e2baec8b3106ae184f1f3c98fd1b6d22
Noah Kim <dongwoo_333@hanmail.net>
2023-05-12 16:56:07 +1000

    67. Adding a Like System

--- e-commerce-app/rbay/src/services/keys.ts
+++ e-commerce-app/rbay/src/services/keys.ts
@@ -3,3 +3,4 @@ export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = 'usernames:unique';
+export const userLikesKey = (userId: string) => `users:${userId}:likes`;
--- e-commerce-app/rbay/src/services/queries/likes.ts
+++ e-commerce-app/rbay/src/services/queries/likes.ts
@@ -1,9 +1,16 @@
+import { client } from '$services/redis';
+import { userLikesKey } from '$services/keys';
+
 export const userLikesItem = async (itemId: string, userId: string) => {};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+	await client.sAdd(userLikesKey(userId), itemId);
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+	await client.sRem(userLikesKey(userId), itemId);
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
