5342e453d4eef836146acf16d7af8c00fb6d662f
Noah Kim <dongwoo_333@hanmail.net>
2023-05-12 17:07:22 +1000

    69. Updating Like Quantities

--- e-commerce-app/rbay/src/services/queries/likes.ts
+++ e-commerce-app/rbay/src/services/queries/likes.ts
@@ -1,18 +1,26 @@
 import { client } from '$services/redis';
-import { userLikesKey } from '$services/keys';
+import { userLikesKey, itemsKey } from '$services/keys';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
-	return client.sIsMember(userLikesKey(userId), itemId);
+	return client.sIsMember(userLikesKey(userId), itemId); // true or false
 };
 
 export const likedItems = async (userId: string) => {};
 
 export const likeItem = async (itemId: string, userId: string) => {
-	await client.sAdd(userLikesKey(userId), itemId);
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+	if (inserted) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+	}
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-	await client.sRem(userLikesKey(userId), itemId);
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+
+	if (removed) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+	}
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
