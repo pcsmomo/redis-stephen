c70d609e5ab426985b4b11254caa7c2818066b1a
Noah Kim <dongwoo_333@hanmail.net>
2023-05-14 13:10:07 +1000

    90. Relational Data with SORT

--- e-commerce-app/rbay/src/services/queries/items/by-views.ts
+++ e-commerce-app/rbay/src/services/queries/items/by-views.ts
@@ -1,1 +1,11 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	const results = await client.sort(itemsByViewsKey, {
+		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+		BY: 'score'
+	});
+
+	console.log(results);
+};
