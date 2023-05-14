4582f1037c4ff9b48387adbdfa2793d1e1d5cce1
Noah Kim <dongwoo_333@hanmail.net>
2023-05-14 11:47:54 +1000

    86. Incrementing View Counters

--- e-commerce-app/rbay/src/services/queries/views.ts
+++ e-commerce-app/rbay/src/services/queries/views.ts
@@ -1,1 +1,9 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const incrementView = async (itemId: string, userId: string) => {
+	return Promise.all([
+		client.hIncrBy(itemsKey(itemId), 'views', 1),
+		client.zIncrBy(itemsByViewsKey, 1, itemId)
+	]);
+};
