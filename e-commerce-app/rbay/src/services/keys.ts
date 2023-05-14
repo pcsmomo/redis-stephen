a216a28a90a6d656ec190369b242eab9060a447f
Noah Kim <dongwoo_333@hanmail.net>
2023-05-14 11:39:19 +1000

    85. Initializing Views on Item Creation

--- e-commerce-app/rbay/src/services/keys.ts
+++ e-commerce-app/rbay/src/services/keys.ts
@@ -1,7 +1,10 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
-export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = 'usernames:unique';
 export const userLikesKey = (userId: string) => `users:${userId}:likes`;
 export const usernamesKey = 'usernames';
+
+// Items
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsByViewsKey = 'items:views';
--- e-commerce-app/rbay/src/services/queries/items/items.ts
+++ e-commerce-app/rbay/src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -34,11 +34,15 @@ export const getItems = async (ids: string[]) => {
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
 
-	console.log(attrs);
-
 	const serialized = serialize(attrs);
 
-	await client.hSet(itemsKey(id), serialized);
+	await Promise.all([
+		client.hSet(itemsKey(id), serialized),
+		client.zAdd(itemsByViewsKey, {
+			value: id,
+			score: 0
+		})
+	]);
 
 	return id;
 };
