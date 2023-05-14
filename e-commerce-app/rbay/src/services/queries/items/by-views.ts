016f0f03ef228f39500016fe7cb2b629b31b0fc7
Noah Kim <dongwoo_333@hanmail.net>
2023-05-15 08:07:24 +1000

    96. Parsing SORT Output

--- e-commerce-app/rbay/src/services/queries/items/by-views.ts
+++ e-commerce-app/rbay/src/services/queries/items/by-views.ts
@@ -1,9 +1,17 @@
 import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-	const results = await client.sort(itemsByViewsKey, {
-		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+	let results: any = await client.sort(itemsByViewsKey, {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
 		BY: 'nosort',
 		// technically there's no 'score' field, so it won't sort by anything
 		// however, sorted sets are sorted by score by default
@@ -14,5 +22,13 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
 		}
 	});
 
-	console.log(results);
+	const items = [];
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
 };
