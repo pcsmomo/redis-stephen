96b8d5a13e55e57fcd26932942c94054c01b68ff
Noah Kim <dongwoo_333@hanmail.net>
2023-05-15 20:53:46 +1000

    109. Retrieving Bid Histories

--- e-commerce-app/rbay/src/services/queries/bids.ts
+++ e-commerce-app/rbay/src/services/queries/bids.ts
@@ -3,10 +3,19 @@ import { bidHistoryKey } from '$services//keys';
 import { client } from '$services/redis';
 import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	const startIndex = -1 * offset - count;
+	const endIndex = -1 - offset;
+
+	const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+
+	return range.map((bid) => deserializeHistory(bid));
 };
 
 const serializeHistory = (amount: number, createdAt: number) => {
