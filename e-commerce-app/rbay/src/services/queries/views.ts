aa1f2168686cdac7636fa1bbfb3ab654d4655dfd
Noah Kim <dongwoo_333@hanmail.net>
2023-05-17 08:25:36 +1000

    132. Creating a View-Incrementing Script

--- e-commerce-app/rbay/src/services/queries/views.ts
+++ e-commerce-app/rbay/src/services/queries/views.ts
@@ -1,13 +1,27 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	// it is slightly better to use Lua Script in terms of concurrency issues
+	return client.incrementView(itemId, userId);
 
-	if (inserted) {
-		return Promise.all([
-			client.hIncrBy(itemsKey(itemId), 'views', 1),
-			client.zIncrBy(itemsByViewsKey, 1, itemId)
-		]);
-	}
+	// By code instead of Lua Script
+	// const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+
+	// if (inserted) {
+	// 	return Promise.all([
+	// 		client.hIncrBy(itemsKey(itemId), 'views', 1),
+	// 		client.zIncrBy(itemsByViewsKey, 1, itemId)
+	// 	]);
+	// }
 };
+
+// Keys I need to access
+// 1) itemsViewsKey ->
+// 2) itemsKey -> items#asldkfjqwe
+// 3) itemsByViewsKey
+//
+// Arguments I need to accept
+// 1) itemId
+// 2) userId
+//
+// EVALSHA <ID> 3 'KEY1' 'KEY2' 'KEY3' 'ARG1' 'ARG2'
--- e-commerce-app/rbay/src/services/redis/client.ts
+++ e-commerce-app/rbay/src/services/redis/client.ts
@@ -1,5 +1,6 @@
 import { RedisCommandArguments } from '@node-redis/client/dist/lib/commands';
 import { createClient, defineScript } from 'redis';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 const client = createClient({
 	socket: {
@@ -21,6 +22,33 @@ const client = createClient({
 			transformReply(reply: any) {
 				return reply;
 			}
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+				local itemsViewsKey = KEYS[1]
+				local itemsKey = KEYS[2]
+				local itemsByViewsKey = KEYS[3]
+				local itemId = ARGV[1]
+				local userId = ARGV[2]
+
+				local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+				if inserted == 1 then
+					redis.call('HINCRBY', itemsKey, 'views', 1)
+					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+				end
+			`,
+			transformArguments(itemId: string, userId: string): RedisCommandArguments {
+				return [
+					itemsViewsKey(itemId), // -> items:views#asdf
+					itemsKey(itemId), // -> items#asdf
+					itemsByViewsKey, // -> items:views
+					itemId, // -> asdf
+					userId // -> u123
+				];
+			},
+			transformReply() {}
 		})
 	}
 });
