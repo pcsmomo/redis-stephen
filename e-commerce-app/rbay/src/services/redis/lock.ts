c44dc2c10747f3f3f8ce762a6d18d1a1f56fbd91
Noah Kim <dongwoo_333@hanmail.net>
2023-05-17 16:47:47 +1000

    138. Implementing WithLock

--- e-commerce-app/rbay/src/services/redis/lock.ts
+++ e-commerce-app/rbay/src/services/redis/lock.ts
@@ -1,4 +1,38 @@
-export const withLock = async () => {};
+import { client } from './client';
+import { randomBytes } from 'crypto';
+
+export const withLock = async (key: string, cb: () => any) => {
+	// Initialize a few variables to control retry behavior
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	// Generate a random value to store at the lock key
+	const token = randomBytes(6).toString('hex'); // 12 characters
+	// Create the lock key
+	const lockKey = `lock:${key}`;
+
+	// Set up a while loop to implement the retry behavior
+	while (retries >= 0) {
+		retries--;
+		// Try to do a SET NX operation
+		const acquired = await client.set(lockKey, token, {
+			NX: true
+		});
+
+		if (!acquired) {
+			// ELSE brief pause (retryDelayMs) and then retry
+			await pause(retryDelayMs);
+			continue;
+		}
+
+		// If the set is successful, then run the callback
+		const result = await cb();
+
+		// Unset the locked set
+		await client.del(lockKey);
+		return result;
+	}
+};
 
 const buildClientProxy = () => {};
 
