496f0259a92ddd6faf1bbc0782b5b58e7b91e973
Noah Kim <dongwoo_333@hanmail.net>
2023-05-20 08:21:40 +1000

    164. Parsing the Search Term

--- e-commerce-app/rbay/src/services/queries/items/search.ts
+++ e-commerce-app/rbay/src/services/queries/items/search.ts
@@ -1,1 +1,12 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemsIndexKey } from '$services/keys';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	const cleaned = term
+		.replaceAll(/[^a-zA-Z0-9 ]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' ');
+};
