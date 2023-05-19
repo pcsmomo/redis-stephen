3d2d1e4c3f4b7c436b357c0ce0a889e539e420f2
Noah Kim <dongwoo_333@hanmail.net>
2023-05-20 08:28:58 +1000

    /Users/noah/Documents/study/study_codes/udemy/redis-ste

--- e-commerce-app/rbay/src/services/queries/items/search.ts
+++ e-commerce-app/rbay/src/services/queries/items/search.ts
@@ -9,4 +9,20 @@ export const searchItems = async (term: string, size: number = 5) => {
 		.split(' ')
 		.map((word) => (word ? `%${word}%` : ''))
 		.join(' ');
+
+	// Look at cleaned and make sure it is valid
+	if (cleaned === '') {
+		return [];
+	}
+
+	// Use the client to do an actual search
+	const results = await client.ft.search(itemsIndexKey, cleaned, {
+		LIMIT: {
+			from: 0,
+			size
+		}
+	});
+
+	// Deserialize and return the research results
+	return results.documents.map(({ id, value }) => deserialize(id, value as any));
 };
